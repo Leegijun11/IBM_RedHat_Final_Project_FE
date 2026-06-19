cc31dafc700c9c12d3d14f5ccfc65a929e3fce16
arim <kwonarim5@gmail.com>
2026-06-19 17:57:51 +0900

    AI 팁 Code 작성

---
+++ src/components/Tip/tip_detail.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { getTipDetail } from "../../services/tip_api";
+
+function Tip_detail() {
+    const [t_id, setT_id] = useState("");
+    const [b_birth, setB_birth] = useState("");
+    const [tip, setTip] = useState(null);
+
+    // 팁 상세 조회
+    const handleGetTipDetail = async () => {
+        
+        try {
+            const result = await getTipDetail (t_id, b_birth);
+
+            console.log(result);
+
+          setTip(result);  
+        } catch (error) {
+            console.log(error);
+
+            alert("Tip 정보를 불러오는데 실패하였습니다.");
+        }
+    };
+
+    return (
+        <div>
+            <h2>AI 발달 팁 상세</h2>
+
+            <input type="number" placeholder="팁 ID" value={t_id} onChange={(e) => setB_birth(e.target.value)} />
+
+            <button onClick={handleGetTipDetail}>조회</button>
+
+            {Tip && (
+                <div>
+                    <h3>{tip.t_title}</h3>
+
+                    <p>{tip.t_age}개월</p>
+
+                    <p>{tip.t_content}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Tip_detail;
