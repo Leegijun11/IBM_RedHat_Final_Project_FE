9af8208089becd09f02ee76c5745de7a96e69b71
이종훈 <ljh11442@gmail.com>
2026-07-18 22:50:04 +0900

    contexts/ModalContext.jsx 생성

---
+++ src/contexts/ModalContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+import AlertModal from "../components/common/AlertModal";
+
+export const ModalContext = createContext();
+
+export function ModalProvider({ children }) {
+  const [modal, setModal] = useState(null);
+
+  const showAlert = (message) => {
+    return new Promise((resolve) => {
+      setModal({
+        message,
+        showCancel: false,
+        onConfirm: () => {
+          setModal(null);
+          resolve(true);
+        },
+      });
+    });
+  };
+
+  const showConfirm = (message) => {
+    return new Promise((resolve) => {
+      setModal({
+        message,
+        showCancel: true,
+        onConfirm: () => {
+          setModal(null);
+          resolve(true);
+        },
+        onCancel: () => {
+          setModal(null);
+          resolve(false);
+        },
+      });
+    });
+  };
+
+  return (
+    <ModalContext.Provider value={{ showAlert, showConfirm }}>
+      {children}
+      {modal && <AlertModal {...modal} />}
+    </ModalContext.Provider>
+  );
+}
