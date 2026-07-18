1da1997f225f261d39367f18f97de8359b323d58
이종훈 <ljh11442@gmail.com>
2026-07-18 22:29:18 +0900

    모달 작업 시작 전

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+console.log("ENV CHECK:", import.meta.env.VITE_API_BASE_URL);
+
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
-        "Content-Type" : "application/json",
+        "Content-Type": "application/json",
     },
     withCredentials: true,
 });
