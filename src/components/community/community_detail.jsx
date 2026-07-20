3575ebfdf7964c5a1622a8b9b1bcb4e7e016d4ef
{name} <{email}>
2026-07-20 18:19:17 +0900

    병합

--- src/components/community/community_detail.jsx
+++ src/components/community/community_detail.jsx
@@ -4,7 +4,7 @@ import useAuth from "../../hooks/useAuth";
 import { getCommunityDetail,toggleCommunityLike,deleteCommunity, getComments, createComments,createCommentLike,deleteCommentLike } from "../../services/community_api";
 import { getImageUrl } from "../../hooks/imageUrl";
 import { useModal } from "../../hooks/useModal";
-import CommentCard from "../../components/Community/Comment_card"; 
+import CommentCard from "../../components/community/Comment_card"; 
 import "../../styles/community_detail.css"; 
 
 const CommunityDetail = () => {
